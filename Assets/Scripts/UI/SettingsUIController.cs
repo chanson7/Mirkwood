412186e98ec0b8affb53dcdc83df058440240238
chanson7 <Hanson.chris@outlook.com>
2023-10-18 15:17:35 -0600

    Made Settings screen a Prefab

--- Assets/Scripts/UI/SettingsUIController.cs
+++ Assets/Scripts/UI/SettingsUIController.cs
@@ -3,12 +3,40 @@ using TMPro;
 
 public class SettingsUIController : MonoBehaviour
 {
+    private static SettingsUIController singleton;
 
-    TMP_Dropdown fullScreenMode;
+    [Header("Player Preferences")]
+
+    [SerializeField] TMP_Dropdown screenModeDropdown;
+
+    bool InitializeSingleton()
+    {
+        if (singleton != null)
+        {
+            Debug.LogWarning("Settings UI already exists in the scene!");
+            return false;
+        }
+
+        singleton = this;
+
+        return true;
+    }
+
+    public void OpenSettingsMenu()
+    {
+        if (!InitializeSingleton()) return;
+
+        Instantiate(this);
+    }
 
     public void UpdateFullScreenMode()
     {
-        Screen.fullScreenMode = (FullScreenMode)fullScreenMode.value;
+        Screen.fullScreenMode = (FullScreenMode)screenModeDropdown.value;
+        Debug.Log($"Screen Mode set to: {(FullScreenMode)screenModeDropdown.value}");
     }
 
+    private void OnDestroy()
+    {
+        singleton = null;
+    }
 }
